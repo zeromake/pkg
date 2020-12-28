e0582f69ede3dae8f18a1377eadbb6a7fcf6e048
zeromake <a390720046@gmail.com>
2020-12-28 16:28:20 +0800

    fix: update text conv split commit

--- textconv/split.go
+++ textconv/split.go
@@ -23,13 +23,21 @@ package textconv
 // SplitString 分割字符串 0-9a-zA-Z 的词语
 func SplitString(input string) []string {
 	var (
+		// 切割后的结果字符串数组
 		result     []string
+		// 当前字符为大写
 		currCase   bool
+		// 当前字符为数字
 		currNumber bool
+		// 下一个字符为大写
 		nextCase   bool
+		// 上一个字符为大写
 		prevCase   bool
+		// 上一个字符为数字
 		prevNumber bool
+		// 需要分割字符串的长度
 		offset     int
+		// 输入字符串长度 -1
 		length     = len(input) - 1
 	)
 	for i := 0; i < length; i++ {
@@ -39,14 +47,18 @@ func SplitString(input string) []string {
 		currNumber = !currCase && v >= '0' && v <= '9'
 		nextCase = nextV >= 'A' && nextV <= 'Z'
 		if currCase || currNumber || (v >= 'a' && v <= 'z') {
+			// 当前字符为字母或数字
 			if offset > 0 {
+				// 有需要切割的字符串
 				if ((!prevCase || prevNumber) && currCase) || (prevCase && currCase && !nextCase) {
+					// 满足 [a-z0-9][A-Z] 或 [A-Z][A-Z][a-z] 情况
 					result = append(result, input[i-offset:i])
 					offset = 0
 				}
 			}
 			offset++
 		} else if offset > 0 {
+			// 碰上不为字母或数字
 			result = append(result, input[i-offset:i])
 			offset = 0
 		}
@@ -54,7 +66,7 @@ func SplitString(input string) []string {
 		prevNumber = currNumber
 	}
 	v := input[length]
-	if (v >= 'A' && v <= 'Z') || (v >= 'a' && v <= 'z') || (v >= '0' && v <= '9') {
+	if nextCase || (v >= 'a' && v <= 'z') || (v >= '0' && v <= '9') {
 		length++
 		offset++
 	}
